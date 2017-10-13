ee2522f5ce40d54036ecfafde0910f7018dcf2f1
Kat Landreth <katlandreth@gmail.com>
2017-10-13 11:53:26 +0700

    updates migration that couldn't cast string to float

--- db/migrate/20170814185309_change_country_scores_to_float.rb
+++ db/migrate/20170814185309_change_country_scores_to_float.rb
@@ -1,11 +1,11 @@
 class ChangeCountryScoresToFloat < ActiveRecord::Migration
   def up
-   change_column :countries, :reading_score, :float
-   change_column :countries, :science_score, :float
-   change_column :countries, :math_score, :float
-   change_column :countries, :life_satisfaction_score, :float
-   change_column :countries, :freedom_of_press_score, :float
-   change_column :countries, :cost_of_living_score, :float
+   change_column :countries, :reading_score, 'double precision USING CAST(reading_score AS double precision)'
+   change_column :countries, :science_score, 'double precision USING CAST(science_score AS double precision)'
+   change_column :countries, :math_score, 'double precision USING CAST(math_score AS double precision)'
+   change_column :countries, :life_satisfaction_score, 'double precision USING CAST(life_satisfaction_score AS double precision)'
+   change_column :countries, :freedom_of_press_score, 'double precision USING CAST(freedom_of_press_score AS double precision)'
+   change_column :countries, :cost_of_living_score, 'double precision USING CAST(cost_of_living_score AS double precision)'
  end
 
  def down
