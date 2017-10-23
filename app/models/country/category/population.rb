406f818f3e8fc8abbd7eee13e83fd39f90a7d432
Kat Landreth <katlandreth@gmail.com>
2017-10-23 12:02:37 +0700

    adds methods to population and population_density classes

--- app/models/country/category/population.rb
+++ app/models/country/category/population.rb
@@ -3,4 +3,29 @@ class Country::Category::Population < ActiveRecord::Base
   def self.unit
     "people"
   end
+
+  def self.slider_min
+    Country.pluck(:population).reject{|s| s == nil }.min
+  end
+
+  def self.slider_max
+    Country.pluck(:population).reject{|s| s == nil }.max
+  end
+
+  def self.slider_initial_values
+    [slider_min, slider_max]
+  end
+
+  def self.slider_input_id
+    "population_range_input"
+  end
+
+  def self.slider_div_id
+    "population_range_div"
+  end
+
+  def self.category_name
+    "Population"
+  end
+
 end
--- app/models/country/category/population_density.rb
+++ app/models/country/category/population_density.rb
@@ -3,4 +3,29 @@ class Country::Category::PopulationDensity < ActiveRecord::Base
   def self.unit
     "People/KM^2"
   end
+
+  def self.slider_min
+    Country.pluck(:population_density).reject{|s| s == nil }.min
+  end
+
+  def self.slider_max
+    Country.pluck(:population_density).reject{|s| s == nil }.max
+  end
+
+  def self.slider_initial_values
+    [slider_min, slider_max]
+  end
+
+  def self.slider_input_id
+    "population_density_range_input"
+  end
+
+  def self.slider_div_id
+    "population_density_range_div"
+  end
+
+  def self.category_name
+    "Population Density"
+  end
+
 end
