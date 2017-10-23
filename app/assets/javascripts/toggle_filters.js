fc4121fed6b265ea0388ee3bd356700427bb07f9
Kat Landreth <katlandreth@gmail.com>
2017-10-23 16:37:52 +0700

    uses jquery 'on()' to bind click event to the filter wrapper so it doesn't break when the filter form is ajaxed

--- app/assets/javascripts/toggle_filters.js
+++ app/assets/javascripts/toggle_filters.js
@@ -1,23 +1,33 @@
-$(function() {
-  $('#showCategorySwitches').click(function() {
-    $('div[id^=category]').not('#category_switches').addClass("hide-filter");
-    $('#category_switches').toggleClass("hide-filter");
-  });
-  $('#showCategoryDealBreakers').click(function() {
-    $('div[id^=category]').not('#category_deal_breakers').addClass("hide-filter");
-    $('#category_deal_breakers').toggleClass("hide-filter");
-  });
+$(document).ready(function(){
+  $(function() {
+  
+    $(".filter-form-wrapper").on("click", "#submit-filter-button", function(){
+      console.log("button clicked");
+      $('div[id^=category]').addClass("hide-filter");
+    });
 
-  $('#showCategoryWeights').click(function() {
-    $('div[id^=category]').not('#category_weights').addClass("hide-filter");
-    $('#category_weights').toggleClass("hide-filter");
-  });
+    $('#showCategorySwitches').click(function() {
+      $('div[id^=category]').not('#category_switches').addClass("hide-filter");
+      $('#category_switches').toggleClass("hide-filter");
+    });
 
-  $('#submit-filter-button').click(function(){
-    $('div[id^=category]').addClass("hide-filter");
+    $('#showCategoryDealBreakers').click(function() {
+      $('div[id^=category]').not('#category_deal_breakers').addClass("hide-filter");
+      $('#category_deal_breakers').toggleClass("hide-filter");
+    });
+
+
+    $('#showCategoryWeights').click(function() {
+      $('div[id^=category]').not('#category_weights').addClass("hide-filter");
+      $('#category_weights').toggleClass("hide-filter");
+    });
+
+    // $('#submit-filter-button').click(function(){
+    //   console.log("button clicked");
+    //   $('div[id^=category]').addClass("hide-filter");
+    // })
   })
-})
-
+});
 
 $(document).ready(function(){
   $('body').on('click', '.btn.active', function(e){
