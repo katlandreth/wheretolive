f5c04263682895a99ccdf4a3f07a528c0cb8e4c4
Kat Landreth <katlandreth@gmail.com>
2017-10-23 11:02:38 +0700

    add the rest of the Country attrs to the CATEGORY constant

--- config/initializers/my_constants.rb
+++ config/initializers/my_constants.rb
@@ -1,4 +1,4 @@
-CATEGORIES            = %w[education_score life_satisfaction_score freedom_of_press_score cost_of_living_score]
+CATEGORIES            = %w[education_score life_satisfaction_score freedom_of_press_score cost_of_living_score population population_density]
 RAWSCORETABLES        = %w[RawReadingScore RawScienceScore RawMathScore RawLifeSatisfactionScore RawFreedomOfPressScore RawCostOfLivingScore]
 RAWCOUNTRYDATATABLES  = %w[Country::Code Country::Category::Population Country::Category::PopulationDensity]
 RAWSCOREANDDATATABLES = RAWSCORETABLES + RAWCOUNTRYDATATABLES
