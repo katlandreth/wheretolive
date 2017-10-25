00e272ccd518681d3c6c25d4748c17da9da28436
Kat Landreth <katlandreth@gmail.com>
2017-10-25 16:35:27 +0700

    adds an overlay while using filters

--- app/assets/javascripts/toggle_filters.js
+++ app/assets/javascripts/toggle_filters.js
@@ -2,24 +2,27 @@ $(document).ready(function(){
   $(function() {
 
     $(".filter-form-wrapper").on("click", "#submit-filter-button", function(){
-      console.log("button clicked");
+      $('.modal-overlay').addClass("mo-hidden");
       $('div[id^=category]').addClass("hide-filter");
     });
 
     $('#showCategorySwitches').click(function() {
       $('div[id^=category]').not('#category_switches').addClass("hide-filter");
       $('#category_switches').toggleClass("hide-filter");
+      toggleOverlay();
     });
 
     $('#showCategoryDealBreakers').click(function() {
       $('div[id^=category]').not('#category_deal_breakers').addClass("hide-filter");
       $('#category_deal_breakers').toggleClass("hide-filter");
+      toggleOverlay();
     });
 
 
     $('#showCategoryWeights').click(function() {
       $('div[id^=category]').not('#category_weights').addClass("hide-filter");
       $('#category_weights').toggleClass("hide-filter");
+      toggleOverlay();
     });
 
   })
@@ -34,3 +37,17 @@ $(document).ready(function(){
       $('input:radio[name="options"]', this).prop('checked', false);
   })
 })
+
+
+function toggleOverlay(){
+  var filterModalsCount = $('.hide-filter').length;
+
+  if( filterModalsCount < 3 ) {
+    $('.modal-overlay').removeClass("mo-hide");
+    $('.modal-overlay').addClass("mo-show");
+  }
+  else {
+    $('.modal-overlay').removeClass("mo-show");
+    $('.modal-overlay').addClass("mo-hide");
+  }
+};
